0126d7c70b754ce9796a0cd0a4c829c681b674a5
engrnisarahmed <cengr.nisar.ahmed@gmail.com>
2022-11-15 21:41:26 +0500

    First Push from eclipse

---
+++ src/main/java/com/rest/example/LogData.java
@@ -0,0 +1,57 @@
+package com.rest.example;
+
+public class LogData {
+
+	private int id;
+	private String coderepogroup;
+	private String coderepofolf;
+	private String coderepoliason;
+	private String coderepolevy;
+	
+	
+	
+	public LogData() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public LogData(int id, String coderepogroup, String coderepofolf, String coderepoliason, String coderepolevy) {
+		super();
+		this.id = id;
+		this.coderepogroup = coderepogroup;
+		this.coderepofolf = coderepofolf;
+		this.coderepoliason = coderepoliason;
+		this.coderepolevy = coderepolevy;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCoderepogroup() {
+		return coderepogroup;
+	}
+	public void setCoderepogroup(String coderepogroup) {
+		this.coderepogroup = coderepogroup;
+	}
+	public String getCoderepofolf() {
+		return coderepofolf;
+	}
+	public void setCoderepofolf(String coderepofolf) {
+		this.coderepofolf = coderepofolf;
+	}
+	public String getCoderepoliason() {
+		return coderepoliason;
+	}
+	public void setCoderepoliason(String coderepoliason) {
+		this.coderepoliason = coderepoliason;
+	}
+	public String getCoderepolevy() {
+		return coderepolevy;
+	}
+	public void setCoderepolevy(String coderepolevy) {
+		this.coderepolevy = coderepolevy;
+	}
+	
+	
+}
